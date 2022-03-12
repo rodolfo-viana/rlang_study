b8526fc0b1dbfe87ae047d411aa01559c858f159
Rodolfo Viana <25807981+rodolfo-viana@users.noreply.github.com>
2022-03-12 12:49:20 -0300

    while loop

---
+++ lesson03/lesson03.R
@@ -0,0 +1,26 @@
+# While loop
+
+# Syntax:
+# while(logical_expr){
+#   operation
+# }
+
+counter <- 0
+while(counter < 12){
+  print(counter) # To print, must use print() only when within a loop
+  counter <- counter + 1
+}
+
+# OUTPUT:
+# [1] 0
+# [1] 1
+# [1] 2
+# [1] 3
+# [1] 4
+# [1] 5
+# [1] 6
+# [1] 7
+# [1] 8
+# [1] 9
+# [1] 10
+# [1] 11
