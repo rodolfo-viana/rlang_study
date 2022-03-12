c21f6beb65f3170a97ba9dc65ed6bf540e4328d6
Rodolfo Viana <25807981+rodolfo-viana@users.noreply.github.com>
2022-03-12 13:01:13 -0300

    for loop

---
+++ lesson04/lesson04.R
@@ -0,0 +1,17 @@
+# for loop
+
+# Syntax:
+# for(elem in array of elems){
+#   operation
+# }
+
+for(i in 1:5){ # 1:5 = vector of numbers from 1 to 5
+  print(i)
+}
+
+# OUTPUT:
+# [1] 1
+# [1] 2
+# [1] 3
+# [1] 4
+# [1] 5
