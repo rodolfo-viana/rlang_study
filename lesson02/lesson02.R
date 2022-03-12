da8df6ed2478a69d60c90dd8ceab4935df2fbb7a
Rodolfo Viana <25807981+rodolfo-viana@users.noreply.github.com>
2022-03-12 12:17:49 -0300

    Relational, bitwise ops

---
+++ lesson02/lesson02.R
@@ -0,0 +1,9 @@
+# Logical vars: TRUE/T, FALSE/F
+
+# Relational ops: ==, !=, <, >, <=, >=, 
+# Logical ops: !, |, &
+
+4 < 5 # OUTPUT: [1] TRUE
+!4 < 5 # [1] FALSE
+(4 < 5) & (1 > 2) # OUTPUT: [1] FALSE
+(4 < 5) | (1 > 2) # OUTPUT: [1] TRUE
